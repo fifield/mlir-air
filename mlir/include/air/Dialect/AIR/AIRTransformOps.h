7d8b3a4c8768b3d0f16c4e5ff1e22e1b36ba9ab2
Jeff Fifield <jeff.fifield@xilinx.com>
2023-01-13 10:35:21 -0700

    Add more transform ops

--- mlir/include/air/Dialect/AIR/AIRTransformOps.h
+++ mlir/include/air/Dialect/AIR/AIRTransformOps.h
@@ -9,7 +9,10 @@
 #ifndef MLIR_AIR_TRANSFORM_OPS_H
 #define MLIR_AIR_TRANSFORM_OPS_H
 
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/Dialect/PDL/IR/PDLTypes.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/Dialect/Transform/IR/TransformInterfaces.h"
 #include "mlir/IR/OpImplementation.h"
 
